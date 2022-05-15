848322dc691f91450afbc4b4b7e7ae8854a88a82
Juan Hidalgo <nachokal@gmail.com>
2022-05-15 11:10:42 -0500

    code fixed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+document.querySelector("button").addEventListener("click", getBookData)
 
+function getBookData() {
+    let ISBN = document.querySelector("input").value
+    
+    fetch(`https://openlibrary.org/isbn/${ISBN}.json`)
+    .then((response) => response.json())
+    .then((data) => populateWithData(data))
+    .catch((error) => console.log(error))
+}
+
+function populateWithData(data) {
+    document.querySelector(".data").innerHTML = 
+    `
+    <h3>${data.title}</h3>
+    <h4>${data.subtitle}. ${data.publish_date}</h4>
+    <p>${data.description}</p>
+    `
+}
